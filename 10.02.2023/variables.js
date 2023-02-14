0fdae9a01e34c872dda505e1ec3085490f81a93f
janushreedurai <123616906+janushreedurai@users.noreply.github.com>
2023-02-14 17:12:14 +0530

    Add files via upload

---
+++ 10.02.2023/variables.js
@@ -0,0 +1,31 @@
+console.log(typeof 45);
+console.log(10-3+7*10/2);
+var Name="janu"
+console.log("Name: "+ Name);
+Age=10;
+Age;
+//disp();
+var Dept="eee";
+function disp(){
+    console.log("Age of undec initialised var:" + Age);
+}
+console.log("Dept: "+ Dept);
+//var Age=20;
+//console.log("declared Age var "+ Age);
+//disp();
+const obj={Name:"janu",dept:"eee",year:3};
+function dispobjcont(){
+    console.log("obj Name: "+ obj.Name);
+    console.log("obj dept: "+ obj.dept);
+    obj.year=4;
+    console.log("obj year: "+ obj.year);
+}
+dispobjcont();
+const pi=3.14;
+pi;
+console.log("const var value: "+ pi);
+//pi=34;
+var decimalval=2343.75649;
+console.log("toFixed fun used "+ decimalval.toFixed(2));
+console.log("exponent operator-"+ 5**2);
+console.log("power func-"+ Math.pow(5,2));
