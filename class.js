db0d383c619e6c3ed55239f4cf08618fc75fd48d
janushreedurai <123616906+janushreedurai@users.noreply.github.com>
2023-02-23 23:50:37 +0530

    class updated code
    class updated code

--- class.js
+++ class.js
@@ -1,30 +1,4 @@
-<<<<<<< HEAD
-class animal{
-    constructor(name,food){
-        this.name=name;
-        #food;
-        this.food=food;
-    }
-   disp(){
-        console.log(this.name);
-    }
-}
-// var obj1=new animal("cat");
-// obj1.disp();
-class sound extends animal{
-    constructor(name,sound,food)
-    {
-        super(name,food);
-        this.sound=sound;
-    }
-    disp(){
-        console.log(`Animal name: ${this.name}, Animal sound: ${this.sound}`)
-    }
-}
-var obj2=new sound("cat","meow","milk");
-obj2.disp();
-=======
-class animals{
+/*class animals{
     constructor(name){
         this.name=name;
     }
@@ -33,5 +7,44 @@ class animals{
     };
 }
 var obj1=new animals("Cat");
-obj1.disp_names();
->>>>>>> 837d265f18cbc7a722c887c6ef1800205565dfe0
+obj1.disp_names();*/
+
+
+class animal{
+    constructor(name){
+        this.name=name;
+    }
+   disp(){
+        console.log(this.name);
+    }
+}
+// var obj1=new animal("cat");
+// obj1.disp();
+/*Private property*/ 
+class sound extends animal{
+    #food;
+    constructor(name,sound,food)
+    {
+        super(name);
+        this.sound=sound;
+        this.#food=food;
+    }
+    disp(){
+        console.log(`Animal name: ${this.name}, Animal sound: ${this.sound}, Animal food: ${this.#food}`)
+    }
+}
+var obj2=new sound("cat","meow","milk");
+obj2.disp()
+//console.log(obj2.#food);
+// private function
+class class_name{
+     public_func(){
+        this.#private_func();
+    }
+    #private_func(){
+        console.log("private function called from public function")
+    }
+}
+var obj3=new class_name;
+obj3.public_func();
+//obj3.#private_func();
