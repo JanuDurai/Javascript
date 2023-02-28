0f0657457faaac10e419f1868efbe07084c1c499
janushree <janushreedurai@gmail.com>
2023-02-28 07:28:42 +0530

    dom

--- dom.js
+++
@@ -1,6 +0,0 @@
-// window.open("https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model")
-// window.close();
-//window.moveTo(500,500);
-// window.print();
-// window.print("Print the page"); 
-console.log(console);
