5872f9efe3aa996463228744d28ef15b37a29fad
JanuDurai <janu.nagarajan@aspiresys.com>
2024-01-20 15:45:59 +0530

    callback

--- callback.js
+++ callback.js
@@ -23,3 +23,55 @@ function func_call(){
     });
 }
 func_call();
+
+//function Step1(result){
+    // return result+1;
+    // }
+    
+    // function Step2(result){
+    // return result+1;
+    // } 
+    
+    // function Step3(result){
+    // return result+1;
+    // } 
+    
+    // function result_func(result){
+    // var res = Step1(result);
+    // res = Step2(res);
+    // res = Step3(res);
+    // console.log(res);
+    // }
+    // result_func(0);
+    
+    //callback
+    
+    function Step1(result,callback){
+    result++;
+    callback(result);
+    }
+    
+    function Step2(result,callback){
+    result++;
+    callback(result);
+    } 
+    
+    function Step3(result,callback){
+    result++;
+    callback(result);
+    } 
+    
+    function result_func(result)
+    {
+    Step1(result,(result1)=>{
+    Step2(result1,(result2)=>{
+    Step3(result2,(result3)=>{
+    console.log(result3);
+    });
+    })
+    });
+    }
+    
+    result_func(1);
+    
+    
