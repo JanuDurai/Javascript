34478b293cdf5b3c6408c3ec5ee66d35ef69e017
JanuDurai <janu.nagarajan@aspiresys.com>
2024-01-22 14:56:41 +0530

    basic

--- callback.js
+++ callback.js
@@ -1,5 +1,5 @@
 function addition(num1,callback){
-    var result=num1+1;
+    var result=num1+num1;
     callback(result);
 }
 
@@ -46,32 +46,32 @@ func_call();
     
     //callback
     
-    function Step1(result,callback){
-    result++;
-    callback(result);
-    }
+    // function Step1(result,callback){
+    // result++;
+    // callback(result);
+    // }
     
-    function Step2(result,callback){
-    result++;
-    callback(result);
-    } 
+    // function Step2(result,callback){
+    // result++;
+    // callback(result);
+    // } 
     
-    function Step3(result,callback){
-    result++;
-    callback(result);
-    } 
+    // function Step3(result,callback){
+    // result++;
+    // callback(result);
+    // } 
     
-    function result_func(result)
-    {
-    Step1(result,(result1)=>{
-    Step2(result1,(result2)=>{
-    Step3(result2,(result3)=>{
-    console.log(result3);
-    });
-    })
-    });
-    }
+    // function result_func(result)
+    // {
+    // Step1(result,(result1)=>{
+    // Step2(result1,(result2)=>{
+    // Step3(result2,(result3)=>{
+    // console.log(result3);
+    // });
+    // })
+    // });
+    // }
     
-    result_func(1);
+    // result_func(1);
     
     
--- promises.js
+++ promises.js
@@ -1,15 +1,79 @@
-var booked=new Promise((response,reject)=>{
-    let status=false;
-    if(status){
-        response();
-    }
-    else
-        reject();
-  })
-    booked.then(()=>{
-        console.log("You have booked the ticket");
-    })
-    .catch(()=>{
-        console.log("You have not booked the ticket");
-    })
+//Promise response status
+let fetchpromise = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+console.log(fetchpromise);
+// fetchpromise.then((response)=>{
+//     console.log(`Response ${response.status}`);
+// })
+
+// console.log("Requesting");
+
+//Promise response data
+// fetchpromise.then((response)=>{
+//     var responsepromise =response.json();
+//     responsepromise.then((data)=>{
+//         console.log(`json file data - ${data[3].type}`);
+//     })
+// })
+
+//Promise chaining
+// fetchpromise.then((response)=>response.json())
+//             .then((data)=>{
+//                 console.log(`data - ${data[0].image}`);
+//             })
+
+// respondng to errors
+
+// fetchpromise.then((response)=>{
+//     if(!response.ok){
+//         throw new Error(`Response status: ${response.status}`);
+//     }
+//     return response.json();
+// })
+//    .then((data)=>{
+//     console.log(`data- ${data[4].name}`);
+//    });
+
+  
+  //catching error
+//   fetchpromise.then((response)=>{
+//     if(!response.ok){
+//         throw new Error(`Response status: ${response.status}`);
+//     }
+//     return response.json();
+// })
+//    .then((data)=>{
+//     console.log(`data- ${data[4].name}`);
+//    })
+//    .catch((error)=>{
+//     console.error(`Error in the data ${error}`);
+//   });
+
+//
+
+  //multiplepromise
+
+  let fetchpromise2 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found");
+  let fetchpromise3 = fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+//   let fetchpromise3 = fetch("bad-scheme://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json");
+//Promise all
+//   Promise.all([fetchpromise,fetchpromise2,fetchpromise3])
+//             .then((responses)=> {
+//               for(const res of responses){
+//                 console.log(`Response status : ${res.status}`);
+//               }
+//             })
+//             .catch((error)=>{
+//                     console.error(`Error: ${error}`);
+//             })
+
+//Promise any
+
+// Promise.any([fetchpromise,fetchpromise2,fetchpromise3])
+//              .then((response)=>{
+//                 console.log(`Response: ${response.status}`);
+//              })
+//             .catch((error)=>{
+//                 console.error(error);
+//             })
+
 
